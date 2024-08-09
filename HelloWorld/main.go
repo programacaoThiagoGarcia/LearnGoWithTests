d0f72b02b720a77134b700cd649a683d8853f8a2
ThiagoGarcia <thiago.garcia@itsector.pt>
2024-08-09 12:55:49 +0100

    Refactoring code

--- HelloWorld/main.go
+++ HelloWorld/main.go
@@ -7,5 +7,8 @@ func main() {
 }
 
 func Hello(name string) string {
+	if name == "" {
+		return "Hello, world!"
+	}
 	return "Hello, " + name + "!"
 }
--- HelloWorld/main_test.go
+++ HelloWorld/main_test.go
@@ -3,9 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Thiago")
-	want := "Hello, Thiago!"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Thiago")
+		want := "Hello, Thiago!"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("Say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world!"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
-		t.Errorf("got %q want %q", got, want)
+		t.Errorf("got -> %q want -> %q", got, want)
 	}
 }
